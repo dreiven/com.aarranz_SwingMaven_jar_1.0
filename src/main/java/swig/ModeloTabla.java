a031754589663ed2c3dcf39a04094f82f0f2114b
dreiven <albertoarranzv@gmail.com>
2015-11-15 14:24:45 +0100

    1.0
    aplicacion con loguin y entrada a bbdd en local con Jdesigner en java
    swing

---
+++ src/main/java/swig/ModeloTabla.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package swig;
+
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
+import javax.swing.table.DefaultTableModel;
+/**
+ *
+ * @author PC
+ */
+public class ModeloTabla extends DefaultTableModel{
+      
+    
+    
+    
+    public ModeloTabla(Object [][] objeto, String [] columnas) {
+        
+    	//Dimensiona la tabla para la presentación inicial
+//        this.setColumnIdentifiers(columnas);
+    	this.setColumnCount(0);
+    	this.setRowCount(0);
+    
+       
+    }
+    
+    
+     public static void Salir(){
+     System.exit(0);
+ }
+    
+    
+    
+    
+    
+}
